10b96f911536e774f90f0cc02d7051281883fe11
Marco <mtacchino4@gmail.com>
2024-12-10 15:42:38 -0300

    Se agrego la carpeta rest-services con el index.js y los paquetes json

---
+++ rest-services/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express ();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/status', (request, response) => {
+    const status = {
+       'Status': 'Running'
+    };
+    
+    response.send(status);
+});
+
+app.listen(port, () => {
+    console.log("Server Listening on PORT:", port);
+});
