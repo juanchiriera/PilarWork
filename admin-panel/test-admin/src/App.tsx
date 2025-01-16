078416ab5eb4687dfdf6c847d71ad05611ad4b97
Marco <mtacchino4@gmail.com>
2025-01-16 17:25:52 -0300

    Agregado pages de clientes, y modificaciones en sus respectivas rutas

--- admin-panel/test-admin/src/App.tsx
+++ admin-panel/test-admin/src/App.tsx
@@ -5,12 +5,16 @@ import espaciosList from './pages/espacios/espacios-list';
 import espacioShow from './pages/espacios/espacio-show';
 import espacioEdit from './pages/espacios/espacio-edit';
 import espacioCreate from './pages/espacios/espacio-create';
-
+import clientesList from './pages/clientes/clientes-list';
+import clientesShow from './pages/clientes/clientes-show';
+import clienteEdit from './pages/clientes/clientes-edit';
+import clienteCreate from './pages/clientes/clientes-create'
 
 
 export const App = () => (
    <Admin layout={Layout} dataProvider={dataProvider}>
         <Resource name="espacios" list={espaciosList} show= {espacioShow} edit= {espacioEdit} create={espacioCreate}/>
+        <Resource name="clientes" list={clientesList} show={clientesShow} edit={clienteEdit} create={clienteCreate}/>
     </Admin>
 );
 
